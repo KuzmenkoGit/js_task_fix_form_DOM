a3ceb2a51ac4ea59e307f4a6a1e48d931d9fe28e
Mykola Kuzmenko <kolua.kuzmenko@gmail.com>
2025-09-07 23:58:20 +0300

    solution 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,16 +2,21 @@
 
 const inputsForm = document.querySelectorAll('form input');
 
-inputsForm.forEach((input) => {
+inputsForm.forEach((input, index) => {
   const nameInput = input.name;
   const capitalize = (str) => str[0].toUpperCase() + str.slice(1);
 
   const labelElement = document.createElement('label');
 
   labelElement.classList.add('field-label');
-  labelElement.setAttribute('for', input.id);
+
+  if (!input.id) {
+     input.id = `input-${input.name || 'field'}-${index}`;
+  }
+  labelElement.htmlFor = input.id
   labelElement.textContent = nameInput;
 
-  input.before(labelElement);
   input.placeholder = capitalize(nameInput);
+  input.parentElement.append(labelElement)
+
 });
